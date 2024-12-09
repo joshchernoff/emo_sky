c3286a0888396330b83985f0f6fc921f4329d4f9
Josh Чернов <961877+joshchernoff@users.noreply.github.com>
2024-12-09 10:10:09 -0800

    wip: add .env, and req dep

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule EmoSky.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:req, "~> 0.5.7"}
     ]
   end
 
